4a49e7081bab0332603126c2d7730d4baa917301
Cristiano Bento <crisdb2008@gmail.com>
2023-01-09 17:38:49 -0300

    change db

--- db_atlas.js
+++ db_atlas.js
@@ -1,14 +1,15 @@
 if(process.env.NODE_ENV == "production"){
 
-module.exports = {MONGODB_URI: "mongodb+srv://cdb:abcdeF12345@cluster0.mvho6.mongodb.net/equipamentos"},
-//module.exports = {URI: "https://polar-beyond-82520.herokuapp.com/"},
+//module.exports = {MONGODB_URI: "mongodb+srv://cdb:abcdeF12345@cluster0.mvho6.mongodb.net/test"},
+
+module.exports = {MONGODB_URI: 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0.mvho6.mongodb.net/'
++process.env.DB_NAME+'?retryWrites=true&w=majority'},
 {
 useNewUrlParser: true,
 useUnifiedTopology: true
 }
 }else{
 module.exports ={MONGODB_URI:"mongodb://localhost/equipamentos"},
-//module.exports ={URI: "http://127.0.0.1:8081/temps"}
 {
     useNewUrlParser: true,
     useUnifiedTopology: true
