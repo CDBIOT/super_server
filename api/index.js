22157e8ba36522fd2d6e3ca6ac015a0007c38cd2
Cristiano Bento <crisdb2008@gmail.com>
2023-01-21 10:14:04 -0300

    cors chat

--- api/index.js
+++ api/index.js
@@ -10,18 +10,13 @@ const cors = require('cors')
 
 app.use(cors({origin: 'https://iot-seven.vercel.app'}));
 
-app.use((req,res,next) => {
-    
-    res.setHeader("Access-Control-Allow-Origin","*");
-    res.setHeader("Access-Control-Allow-Header",'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS'){
-        res.setHeader('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-        res.status(200).send({})
-    }
-    console.log("Cors habilitado");
-    
-   next()
-   })
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    console.log('Cors habilitado')
+    next();
+});
 //Read
 if(process.env.NODE_ENV == "production"){
 
