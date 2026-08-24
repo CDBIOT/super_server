28d6bcd0facef021e6e3f4f569afdabca6001e46
CDBIOT <cristiab05@yahoo.com.br>
2026-08-24 15:30:09 -0300

    postgres

--- db_pg.js
+++ db_pg.js
@@ -1,37 +1,5 @@
 // Do not expose your Neon credentials to the browser
 // .env
-require('dotenv').config();
-
-const {Pool} = require('pg')
-const { DATABASE_URL } = process.env;
-
- const pool = new Pool({
-  connectionString: DATABASE_URL,
-  ssl:{
-       required: true,
-      rejectUnauthorized: false,
-  },
-})
-async function connect() {
-  if (global.connection)
-  return global.connection.connect();
-  
- {
-  //apenas testando a conexão
- const client = await pool.connect();
- console.log("Criou pool de conexões no PostgreSQL!");
-    
- const res = await client.query('SELECT NOW()');
- console.log(res.rows[0]);
- client.release();
-
- //guardando para usar sempre o mesmo
- global.connection = pool;
- return pool.connect();
-    }
-    
-}
-connect();
 
 //Read from postgre
 const getProducts = (req, res) =>{
