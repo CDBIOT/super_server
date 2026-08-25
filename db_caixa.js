1b6725a194144e37fec44c4709a045b880e4a6bb
CDBIOT <cristiab05@yahoo.com.br>
2026-08-24 23:28:25 -0300

    pg_connect

--- db_caixa.js
+++ db_caixa.js
@@ -2,22 +2,16 @@
 const pool = require('./db_pg_connect');
 
 //Read from postgre
- const getProducts = async (req, res) => {
-    try {
-        const results = await pool.query(
-            'SELECT * FROM products ORDER BY id'
-        );
+const getProducts = (req, res) =>{
+pool.query('SELECT * FROM Products',(error,results)=>{
+if (error){
+   throw error
+}
+res.status(200).json(results.rows)
+})
+  
+}
 
-        res.status(200).json(results.rows);
-
-    } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
-
-        res.status(500).json({
-            error: 'Erro ao buscar produtos'
-        });
-    }
-};
 const postProducts = (req, res)=>{
   const {id,product,marca,price,qtd} = req.body
 pool.query('INSERT INTO Products (id,product, marca, price, qtd) VALUES ($1,$2,$3,$4,$5) RETURNING *',[id,product,marca,price,qtd],(error,results)=>{
--- db_pg_connect.js
+++ db_pg_connect.js
@@ -13,23 +13,5 @@ const { DATABASE_URL } = process.env;
   },
 })
 
-async function connect() {
-  if (global.connection)
-  return global.connection.connect();
-  
- {
-  //apenas testando a conexão
- const client = await pool.connect();
- console.log("Criou pool de conexões no PostgreSQL!");
-    
- const res = await client.query('SELECT NOW()');
- console.log(res.rows[0]);
- client.release();
 
- //guardando para usar sempre o mesmo
- global.connection = pool;
- return pool.connect();
-    }
-    
-}
-connect();
+module.exports = pool;
