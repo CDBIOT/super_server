118a6b0a79c6c19cec080722ce57827c7c79cede
CDBIOT <cristiab05@yahoo.com.br>
2026-08-25 13:10:28 -0300

    sales

--- rotas_sales.js
+++ rotas_sales.js
@@ -1,20 +1,8 @@
 const Sales = require("./db_sales")
-const Products = require('./db_products');
 
 
 //API HTTP dos vendas
 
-const getProducts = async (req, res) => {
-    try {
-        const products = await Products.getProducts();
-        res.status(200).json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            error: 'Erro ao buscar produtos'
-        });
-    }
-};
 
 
 //Read
