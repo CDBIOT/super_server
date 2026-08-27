68accb6cfaa27570fd969bbd2ce3e4bcb06670ab
CDBIOT <cristiab05@yahoo.com.br>
2026-08-26 21:46:03 -0300

    rotas

--- rotas_products.js
+++ rotas_products.js
@@ -57,35 +57,32 @@ const postProducts = (req, res)=>{
 
 
 
-//Update
- const putProducts = (async (req, res) =>{
-    const id = req.params.id
-    const {barcode,product, marca, price, qtd } = req.body
-    const prod = {barcode,product, marca, price, qtd}
-    try{
-     const updateProd = await Products.updateOne({id: id},prod);
-   
-     res.status(200).json( " mensagem: 'Correção executada'",temps);
-    }catch(error){
-    res.status(500).json({error: error})
-    }  
-})
+// //Update
+//  const putProducts = (async (req, res) =>{
 
- //Delete
-const deleteProducts = (async (req, res) => {
-    const id= req.params.id
-    //temps.remove({id: req.body.id})
-    try{
-    await Products.deleteOne({_id: id}) 
-        return res.json({
-            message: "Artigo apagado com sucesso!",
-            id
-            })
-    }catch(error){
-         return res.status(400).json({
-        message: "Error: Artigo não foi apagado com sucesso!"
-    })}
-})
+//     try{
+//     const updateProduct = req.body
+//      res.status(200).json( " mensagem: 'Correção executada'",temps);
+//     }catch(error){
+//     res.status(500).json({error: error})
+//     }  
+// })
+
+//  //Delete
+// const deleteProducts = (async (req, res) => {
+//     const id= req.params.id
+//     //temps.remove({id: req.body.id})
+//     try{
+//     await Products.deleteOne({_id: id}) 
+//         return res.json({
+//             message: "Artigo apagado com sucesso!",
+//             id
+//             })
+//     }catch(error){
+//          return res.status(400).json({
+//         message: "Error: Artigo não foi apagado com sucesso!"
+//     })}
+// })
     
 
 module.exports={
