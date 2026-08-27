8e93ad66f8bd8994f446198b92b1634db525ec53
CDBIOT <cristiab05@yahoo.com.br>
2026-08-26 22:18:46 -0300

    dbcaixa

--- db_caixa.js
+++ db_caixa.js
@@ -1,29 +1,49 @@
 
 const pool = require('./db_pg_connect');
-
-//SQL de Caixa
-const getProducts = (req, res) =>{
-pool.query('SELECT * FROM Products',(error,results)=>{
-if (error){
-   throw error
-}
-res.status(200).json(results.rows)
-})
-  
-}
-
-const postProducts = (req, res)=>{
-  const {id,product,marca,price,qtd} = req.body
-pool.query('INSERT INTO Products (id,product, marca, price, qtd) VALUES ($1,$2,$3,$4,$5) RETURNING *',[id,product,marca,price,qtd],(error,results)=>{
-if (error){
-throw error
-}
-res.status(201).send(`Product inserted ${results.rows[0].id}`)
-})
-}
+const pool = require('./db_pg_connect');
 
 
-module.exports= {getProducts,postProducts}
+// Buscar caixas
+const getCaixa = async () => {
+
+    const result = await pool.query(`
+        SELECT *
+        FROM caixa
+        ORDER BY caixa_id DESC
+    `);
+
+    return result.rows;
+};
 
 
+// Abrir caixa
+const abrirCaixa = async (dados) => {
 
+    const {
+        usuario_id,
+        valor_inicial
+    } = dados;
+
+    const result = await pool.query(`
+        INSERT INTO caixa
+        (
+            usuario_id,
+            data_abertura,
+            valor_inicial,
+            status
+        )
+        VALUES ($1, NOW(), $2, 'ABERTO')
+        RETURNING *
+    `, [
+        usuario_id,
+        valor_inicial
+    ]);
+
+    return result.rows[0];
+};
+
+
+module.exports = {
+    getCaixa,
+    abrirCaixa
+};
